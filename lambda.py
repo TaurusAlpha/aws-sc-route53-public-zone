6f6be506dbcaf85846513d5022780a102c171f16
Andrey Voroshnin <andreyv@dimas-imac-pro.home>
2024-04-21 08:56:49 +0200

    lambda update

--- lambda.py
+++ lambda.py
@@ -1,57 +1,81 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 import boto3
-import cfnresponse
+import cfnresponse  # type: ignore
+
 if TYPE_CHECKING:
-    from mypy.boto3.route53.client import Route53Client
-    from mypy.boto3.sts.client import STSClient
-    
-def lambda_handler(event, context):
+    from mypy_boto3_sts.client import STSClient
+    from mypy_boto3_route53.client import Route53Client
+
+
+def lambda_handler(event, context) -> None:
     try:
         # Setup for accessing Route 53
-        sts_client = boto3.client('sts')
-        assumed_role = sts_client.assume_role(RoleArn="arn:aws:iam::<DESTINATION_ACCOUNT_ID>:role/LambdaRoute53ManagementRole", RoleSessionName="R53UpdateSession")
-        credentials = assumed_role['Credentials']
-        route53 = boto3.client('route53',
-        aws_access_key_id=credentials['AccessKeyId'],
-        aws_secret_access_key=credentials['SecretAccessKey'],
-        aws_session_token=credentials['SessionToken'])
-        ns_records = list(event['ResourceProperties']['NSRecords'].split(","))
-        domain_name = event['ResourceProperties']['DomainName']
-        if event['RequestType'] == 'Delete':
-        # Handling deletion of NS records
-        response = route53.change_resource_record_sets(
-            HostedZoneId='DESTINATION_ZONE_ID', # Change this to your destination hosted zone ID
-            ChangeBatch={
-            'Changes': [{
-                'Action': 'DELETE',
-                'ResourceRecordSet': {
-                'Name': domain_name,
-                'Type': 'NS',
-                'TTL': 300,
-                'ResourceRecords': [{'Value': ns} for ns in ns_records]
-                }
-            }]
-            }
+        sts_client: STSClient = boto3.client("sts")
+        assumed_role = sts_client.assume_role(
+            RoleArn="arn:aws:iam::<DESTINATION_ACCOUNT_ID>:role/LambdaRoute53ManagementRole",
+            RoleSessionName="R53UpdateSession",
         )
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {"Message": "NS records deleted"})
-        return
+        credentials = assumed_role["Credentials"]
+        r53_client: Route53Client = boto3.client(
+            "route53",
+            aws_access_key_id=credentials["AccessKeyId"],
+            aws_secret_access_key=credentials["SecretAccessKey"],
+            aws_session_token=credentials["SessionToken"],
+        )
+        ns_records = event["ResourceProperties"]["NSRecords"].split(",")
+        domain_name = event["ResourceProperties"]["DomainName"]
+        
+        # Handling deletion of NS records
+        if event["RequestType"] == "Delete":
+            response = r53_client.change_resource_record_sets(
+                HostedZoneId=domain_name,
+                ChangeBatch={
+                    "Comment": f"Delete NS records for {domain_name}",
+                    "Changes": [
+                        {
+                            "Action": "DELETE",
+                            "ResourceRecordSet": {
+                                "Name": domain_name,
+                                "Type": "NS",
+                                "TTL": 300,
+                                "ResourceRecords": [{"Value": ns} for ns in ns_records],
+                            },
+                        }
+                    ],
+                },
+            )
+            cfnresponse.send(
+                event,
+                context,
+                cfnresponse.SUCCESS,
+                {"Message": f"NS records {ns_records} deleted"},
+            )
+            return
 
         # Handling creation/updation of NS records
-        response = route53.change_resource_record_sets(
-        HostedZoneId='DESTINATION_ZONE_ID',
-        ChangeBatch={
-            'Changes': [{
-            'Action': 'UPSERT',
-            'ResourceRecordSet': {
-                'Name': domain_name,
-                'Type': 'NS',
-                'TTL': 300,
-                'ResourceRecords': [{'Value': ns} for ns in ns_records]
-            }
-            }]
-        }
+        response = r53_client.change_resource_record_sets(
+            HostedZoneId=domain_name,
+            ChangeBatch={
+                "Comment": f"Update NS records for {domain_name}",
+                "Changes": [
+                    {
+                        "Action": "UPSERT",
+                        "ResourceRecordSet": {
+                            "Name": domain_name,
+                            "Type": "NS",
+                            "TTL": 300,
+                            "ResourceRecords": [{"Value": ns} for ns in ns_records],
+                        },
+                    }
+                ],
+            },
         )
-        cfnresponse.send(event, context, cfnresponse.SUCCESS, {"Message": "NS records updated"})
+        cfnresponse.send(
+            event,
+            context,
+            cfnresponse.SUCCESS,
+            {"Message": f"NS records {ns_records} updated"},
+        )
     except Exception as e:
         cfnresponse.send(event, context, cfnresponse.FAILED, {"Message": str(e)})
