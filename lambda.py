70bc37c9be296248ec6ec2ff8e9197e00284bea8
Andrey Voroshnin <andreyv@comm-it.com>
2025-04-19 12:44:27 +0200

    Refactor lambda_handler to simplify request type handling and remove type hints for boto3 clients

--- lambda.py
+++ lambda.py
@@ -1,23 +1,19 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
 import boto3
 import cfnresponse  # type: ignore
 
-if TYPE_CHECKING:
-    from mypy_boto3_sts.client import STSClient
-    from mypy_boto3_route53.client import Route53Client
-
 
 def lambda_handler(event, context) -> None:
+    request_type = event["RequestType"].toupper()
     try:
         # Setup for accessing Route 53
-        sts_client: STSClient = boto3.client("sts")
+        sts_client = boto3.client("sts")
         assumed_role = sts_client.assume_role(
             RoleArn="arn:aws:iam::<DESTINATION_ACCOUNT_ID>:role/LambdaRoute53ManagementRole",
             RoleSessionName="R53UpdateSession",
         )
         credentials = assumed_role["Credentials"]
-        r53_client: Route53Client = boto3.client(
+        r53_client = boto3.client(
             "route53",
             aws_access_key_id=credentials["AccessKeyId"],
             aws_secret_access_key=credentials["SecretAccessKey"],
@@ -25,10 +21,10 @@ def lambda_handler(event, context) -> None:
         )
         ns_records = event["ResourceProperties"]["NSRecords"].split(",")
         domain_name = event["ResourceProperties"]["DomainName"]
-        
+
         # Handling deletion of NS records
-        if event["RequestType"] == "Delete":
-            response = r53_client.change_resource_record_sets(
+        if request_type == "DELETE":
+            r53_client.change_resource_record_sets(
                 HostedZoneId=domain_name,
                 ChangeBatch={
                     "Comment": f"Delete NS records for {domain_name}",
@@ -51,31 +47,40 @@ def lambda_handler(event, context) -> None:
                 cfnresponse.SUCCESS,
                 {"Message": f"NS records {ns_records} deleted"},
             )
-            return
 
         # Handling creation/updation of NS records
-        response = r53_client.change_resource_record_sets(
-            HostedZoneId=domain_name,
-            ChangeBatch={
-                "Comment": f"Update NS records for {domain_name}",
-                "Changes": [
-                    {
-                        "Action": "UPSERT",
-                        "ResourceRecordSet": {
-                            "Name": domain_name,
-                            "Type": "NS",
-                            "TTL": 300,
-                            "ResourceRecords": [{"Value": ns} for ns in ns_records],
-                        },
-                    }
-                ],
-            },
-        )
-        cfnresponse.send(
-            event,
-            context,
-            cfnresponse.SUCCESS,
-            {"Message": f"NS records {ns_records} updated"},
-        )
+        elif request_type == "CREATE":
+            r53_client.change_resource_record_sets(
+                HostedZoneId=domain_name,
+                ChangeBatch={
+                    "Comment": f"Update NS records for {domain_name}",
+                    "Changes": [
+                        {
+                            "Action": "UPSERT",
+                            "ResourceRecordSet": {
+                                "Name": domain_name,
+                                "Type": "NS",
+                                "TTL": 300,
+                                "ResourceRecords": [{"Value": ns} for ns in ns_records],
+                            },
+                        }
+                    ],
+                },
+            )
+            cfnresponse.send(
+                event,
+                context,
+                cfnresponse.SUCCESS,
+                {"Message": f"NS records {ns_records} updated"},
+            )
+
+        else:
+            cfnresponse.send(
+                event,
+                context,
+                cfnresponse.SUCCESS,
+                {"Message": f"request_type {request_type} not supported"},
+            )
+
     except Exception as e:
         cfnresponse.send(event, context, cfnresponse.FAILED, {"Message": str(e)})
